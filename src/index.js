cbc1a45ae99db97d119266a03e22bd664f779e10
Tomas Gunther <tomasgunther@gmail.com>
2020-10-20 01:28:28 +0000

    feat: chapter three exercise

--- src/index.js
+++ src/index.js
@@ -6,24 +6,52 @@ let links = [{
   description: 'Fullstack tutorial for GraphQL'
 }]
 
-let idCount = 0;
+const getLink = (parent, args) => {
+  return links.find((link) => {
+    return link.id === args.id
+  })
+}
+
+const updateLink = (parent, args) => {
+  const link = getLink(parent, args)
+  if (args.url) {
+    link.url = args.url
+  }
+  if (args.description) {
+    link.description = args.description
+  }
+  return link
+}
+
+const deleteLink = (parent, args) => {
+  const link = getLink(parent, args)
+  links = links.filter((link) => {
+    return link.id !== args.id
+  })
+  return link
+}
+
+let idCount = 0
 const resolvers = {
   Query: {
     info: () => `This is the API of a Hackernews Clone`,
     feed: () => links,
+    get: getLink
   },
   Mutation: {
     post: (parent, args) => {
-       const link = {
+      const link = {
         id: `link-${idCount++}`,
         description: args.description,
         url: args.url,
       }
       links.push(link)
       return link
-    }
+    },
+    update: updateLink,
+    delete: deleteLink
   },
-};
+}
 
 const server = new GraphQLServer({
   // Can receive an object or a string
